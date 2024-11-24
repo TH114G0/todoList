d14b5d90e9aa3f1bc1a54dd0f29fb80ff6ba574b
Thiago Jose <thiago.wilton2704@gmail.com>
2024-11-23 22:23:02 -0300

    Adicionando anotações de validação e construtores na entidade Todo

--- src/main/java/br/com/todo/api/entity/Todo.java
+++ src/main/java/br/com/todo/api/entity/Todo.java
@@ -1,6 +1,7 @@
 package br.com.todo.api.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -12,12 +13,22 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String nome;
 
+    @NotBlank
     private String descricao;
 
     private boolean realizado;
 
     private int prioridade;
 
+    public Todo() {}
+
+    public Todo(String nome, String descricao, boolean realizado, int prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
 }
