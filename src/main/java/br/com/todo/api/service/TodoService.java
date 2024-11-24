815b3ab94ede5a22098894d540c9fbc289865a45
Thiago Jose <thiago.wilton2704@gmail.com>
2024-11-23 23:14:43 -0300

    Pequenos ajustes

--- src/main/java/br/com/todo/api/service/TodoService.java
+++ src/main/java/br/com/todo/api/service/TodoService.java
@@ -4,9 +4,7 @@ import br.com.todo.api.entity.Todo;
 import br.com.todo.api.exception.TodoNotFoundException;
 import br.com.todo.api.repository.TodoRepository;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
